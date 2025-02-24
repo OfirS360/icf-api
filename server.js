a6b4c08abe12dc00090cf90c08ab4e42cd255832
MrBloby1 <132688911+MrBloby1@users.noreply.github.com>
2025-02-25 00:10:16 +0200

    Trigger deployment

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://icf.xitsraz.me");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+
 const db = mysql.createConnection({
     host: "95.217.11.99",
     port: "3307",
@@ -21,6 +29,10 @@ db.connect(err => {
         return;
     }
     console.log("Connected to SQL");
+});
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
 });
 
 app.post("/RegisterFormSend", (req, res) => {
